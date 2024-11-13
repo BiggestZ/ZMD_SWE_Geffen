611d905805f0f4edd31d6c5e9fe217b21be805ad
Danny <dkim5@oxy.edu>
2024-11-12 22:42:23 -0800

    added form submission - not done

--- src/app/addNew/page.tsx
+++ src/app/addNew/page.tsx
@@ -1,41 +1,69 @@
+"use client"
 import TagTable from "../components/tagTable";
+import axios from 'axios';
+
 
 export default function Home() {
-    
+    // Function to handle form submission
+    //FIXME: make api that adds informatin from the form to DB
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault(); // Prevents the default form submission
+    console.log('TESTING SUBMIT')
+    console.log('Form submitted successfully')
+    // try {
+    //   // Using axios to send a POST request
+    //   const response = await axios.post('/api/getTopic', {
+    //     //FIXME
+    //     // Send the form data in the request body
+    //   });
+
+    //   if (response.status === 200) {
+    //     // Handle success
+    //     console.log('Form submitted successfully');
+    //   } else {
+    //     // Handle error response
+    //     console.log('Error submitting form:', response.statusText);
+    //   }
+    // } catch (error) {
+    //   console.error('Error submitting form:', error);
+    // }
+  };
     return (
-        <div>
+        <form onSubmit = {handleSubmit}>
             <h1>add new test </h1>
             <>
         <label>
           isbn:
           <input type = "number" id = "isbn" />
-        </label>
-        <hr />
+          </label>
+       
         <label>
           Title:
           <input title= "title" />
-        </label>
-        <hr />
+          
         <label>
           Author:
           <input name= "name" />
         </label>
+        </label>
+        
         <hr />
 
+        {/* FIXME - TagTable View doesn't clear when reset button is pressed */}
         <TagTable></TagTable>
-        <hr />
-        <button>Search</button>
+        
         <hr />
         
        <button type="reset">Reset form</button>
        <button type="submit">Submit form</button>
         
 
-        {/* <label htmlFor={ageInputId}>Your age:</label>
-        <input id={ageInputId} name="age" type="number" /> */}
+        
         </>
+      
+        
 
-        </div>  
+        </form>  
 
          
     );
