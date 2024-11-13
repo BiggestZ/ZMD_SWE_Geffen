bf6bdbc766fbbeb0465dc32e396e18d979bb46c9
Danny <dkim5@oxy.edu>
2024-11-12 20:12:30 -0800

    initial draft for "add new" page on website

--- src/app/addNew/page.tsx
+++ src/app/addNew/page.tsx
@@ -1,8 +1,43 @@
+import TagTable from "../components/tagTable";
+
 export default function Home() {
+    
     return (
         <div>
-            <h1>add new test</h1>
-        </div>      
+            <h1>add new test </h1>
+            <>
+        <label>
+          isbn:
+          <input type = "number" id = "isbn" />
+        </label>
+        <hr />
+        <label>
+          Title:
+          <input title= "title" />
+        </label>
+        <hr />
+        <label>
+          Author:
+          <input name= "name" />
+        </label>
+        <hr />
+
+        <TagTable></TagTable>
+        <hr />
+        <button>Search</button>
+        <hr />
+        
+       <button type="reset">Reset form</button>
+       <button type="submit">Submit form</button>
+        
+
+        {/* <label htmlFor={ageInputId}>Your age:</label>
+        <input id={ageInputId} name="age" type="number" /> */}
+        </>
+
+        </div>  
+
+         
     );
   }
   
