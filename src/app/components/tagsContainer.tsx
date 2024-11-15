24b3ebdc780dded7c26985cf6147e01e3135e241
mintmaxed <akcheng03@gmail.com>
2024-11-14 17:07:28 -0800

    catching up

---
+++ src/app/components/tagsContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Tag } from "./tag";
+
+const TagsContainer : React.FC = () => {
+    return(
+        <div className="flex flex-wrap flex-row place-content-start p-1 gap-2">
+            <Tag SubtopicName={"sample tag"} SubtopicID={0} TopicID={0} />
+        </div>
+    );
+};
+
+export { TagsContainer };
