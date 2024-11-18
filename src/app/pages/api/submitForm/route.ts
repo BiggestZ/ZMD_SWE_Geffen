16c48fdbcc1efba7ebdca4218d485fd627457d88
Danny <dkim5@oxy.edu>
2024-11-17 16:53:51 -0800

    cleaning up code

--- src/app/pages/api/submitForm/route.ts
+++ src/app/pages/api/submitForm/route.ts
@@ -54,3 +54,17 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
   }
 }
+// import { NextResponse } from 'next/server';
+
+// export async function POST(req: Request) {
+//   const body = await req.json();
+//   const { isbn, title, author, tags } = body;
+
+//   if (!isbn || !title || !author || !tags) {
+//     return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
+//   }
+
+//   console.log('Received data:', { isbn, title, author, tags });
+
+//   return NextResponse.json({ message: 'Form data saved successfully!' });
+// }
